e3c0096eadd7788facddf7983b74a038079bace3
Javierzz2401 <javierzz2401@gmail.com>
2022-12-27 13:26:28 -0500

    UpdateJS1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = null;
 const nuevoNum = 'Hola';
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = 'chao';
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
